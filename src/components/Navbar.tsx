4996027b50ff703b8d039321f3ac62ee3300afd0
Dece1st <FFSaraf12@gmail.com>
2025-09-26 17:32:06 -0400

    Basic NavBar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="bg-gray-800 h-16 flex items-center border-b border-gray-400">
+            <div className="
+                font-serif text-2xl pl-6 bg-gradient-to-r font-bold from-green-500
+                via-green-400 to-green-200 bg-clip-text text-transparent">
+                CampusVoice
+            </div>
+        </div>
+    )
+}
