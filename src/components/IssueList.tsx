14a66787c3cc6b21e60ba392b1360e2e6b6ae816
maida5 <145588313+maida5@users.noreply.github.com>
2025-10-05 15:22:32 -0400

    created basis and styling for the issuelist component

---
+++ src/components/IssueList.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import IssueCard from "./IssueCard";
+import type { IssueCardProps, College, IssueCategory } from "./IssueCard";
+
+type issue = IssueCardProps;
+type FilterOption = IssueCategory | "All";
+type CollegeFilterOption = College | "All";
+
+// future updates: deleting issues, editing issues
+interface IssueListProps {
+   issues: IssueCardProps[];
+   // onDeleteIssue?: (issue: IssueCardProps) => void;
+}
+
+// when user is logged in, set college to their college
+const IssueList: React.FC<IssueListProps> = ({
+   issues,
+   // filterByCollege = "All",
+   // filterByCategory = "All"
+   // onDeleteIssue,
+}) => {
+   const [filterCategory, setFilterCategory] = useState<FilterOption>("All");
+   const [filterCollege, setFilterCollege] =
+      useState<CollegeFilterOption>("All");
+
+   const filteredIssues = () => {
+      if (filterCollege === "All" && filterCategory === "All") {
+         return issues;
+      } else if (filterCollege === "All") {
+         return issues.filter((issue) => issue.category === filterCategory);
+      } else if (filterCategory === "All") {
+         return issues.filter((issue) => issue.college === filterCollege);
+      } else {
+         return issues.filter(
+            (issue) =>
+               issue.college === filterCollege &&
+               issue.category === filterCategory
+         );
+      }
+   };
+
+   // const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+   //    setFilterCategory(e.target.value as FilterOption);
+   // };
+
+   // const handleCollegeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+   //    setFilterCollege(e.target.value as CollegeFilterOption);
+   // };
+
+   return (
+      <div className="bg-gray-700 p-6 rounded-lg border border-gray-600 w-full">
+         <h2 className="text-xl font-semibold text-white mb-4">
+            Recent Issues
+         </h2>
+
+         {filteredIssues().length === 0 ? (
+            <p className="text-gray-400">No issues found.</p>
+         ) : (
+            // UPDATED
+            filteredIssues().map((issue) => (
+               <IssueCard key={issue.id} {...issue} />
+            ))
+         )}
+      </div>
+   );
+};
+
+export default IssueList;
